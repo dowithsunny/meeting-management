ff6a860eebc3bc79eb94a04fa33ed5a212e4cc1e
dowithsunny <mmsunny361@gmail.com>
2024-05-06 11:27:59 +0600

    select between 900 to 1600 available time

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -4,21 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Meeting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MeetingController extends Controller
 {
-    public function addMeeting(Request $request){
-     $meeting = New Meeting;
-     $meeting->user_id = $request->user_id;
-     $meeting->location = $request->location;
-     $meeting->latitude = $request->latitude;
-     $meeting->longitude = $request->longitude;
-     $meeting->meeting_time = $request->time;
-     $meeting->distance_time = $request->dtime;
-     $meeting->distance_km = $request->dkm;
-     $meeting->date = $request->date;
-     $meeting->save();
+    public function addMeeting(Request $request)
+    {
+        $minutes = Meeting::select(DB::raw("SUM(meeting_time) as total_time"))
+            ->where([
+                'user_id' => $request->user_id,
+                'date' => $request->date,
+            ])->get();
 
-     return back()->with("Success","Meeting schedule with client added successfully!");
+        $currentUserMinutes = 9 * 60;
+
+        if ($minutes[0]['total_time'] >= $currentUserMinutes) {
+            return back()->with("error", "On this date " . $request->date . " all schedule are busy, please select another available date.");
+        } elseif (($minutes[0]['total_time'] + $request->time) > $currentUserMinutes) {
+            $currentRemMinutes = $currentUserMinutes - $minutes[0]['total_time'];
+            return back()->with("error", "On this date " . $request->date . " you've only " . $currentRemMinutes . " mintues remain for meeting.");
+        } else {
+            $meeting = new Meeting;
+            $meeting->user_id = $request->user_id;
+            $meeting->location = $request->location;
+            $meeting->latitude = $request->latitude;
+            $meeting->longitude = $request->longitude;
+            $meeting->meeting_time = $request->time;
+            $meeting->distance_time = $request->dtime;
+            $meeting->distance_km = $request->dkm;
+            $meeting->date = $request->date;
+            $meeting->save();
+
+            return back()->with("Success", "Meeting schedule with client added successfully!");
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,6 +44,9 @@
         @if (Session::has('success'))
             <p style="color:green;">{{ Session::get('success') }}</p>
         @endif
+        @if (Session::has('error'))
+            <p style="color:red;">{{ Session::get('error') }}</p>
+        @endif
     </div>
     {{-- Google Place Autocomplete API [replace new key with the old ones] --}}
     <script type="text/javascript"
